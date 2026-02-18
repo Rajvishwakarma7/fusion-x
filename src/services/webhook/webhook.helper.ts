669683c98eef006b7e098c590cd37f2d7158193a
raj.vishwakarma <raj7280021@gmail.com>
2026-02-18 18:13:32 +0530

    feat: update stripe event for manage payment history and subscription

--- src/services/webhook/webhook.helper.ts
+++ src/services/webhook/webhook.helper.ts
@@ -1,11 +1,129 @@
-
-// ***** Old utility function for upserting the sub info ***** //
-export const insertSubscriptionRecord = async (dataInfo: any) => {
+export const handleOneTimePaymentSuccess = async (eventBody: any) => {
   try {
-
+    console.log('test', eventBody);
   } catch (error) {
-    console.log("Getting an error for inserting subscription record :", error);
+    console.log(
+      'Getting an error  :',
+      error
+    );
+    throw error;
   }
 };
+
+
+export const handleSubscriptionSuccess = async (eventBody: any) => {
+  try {
+    console.log('test', eventBody);
+  } catch (error) {
+    console.log(
+      'Getting an error  :',
+      error
+    );
+    throw error;
+  }
+};
+
+
+export const handleSubscriptionCreated = async (eventBody: any) => {
+  try {
+    console.log('test', eventBody);
+  } catch (error) {
+    console.log(
+      'Getting an error  :',
+      error
+    );
+    throw error;
+  }
+};
+
+
+export const handleSubscriptionUpdated = async (eventBody: any) => {
+  try {
+    console.log('test', eventBody);
+  } catch (error) {
+    console.log(
+      'Getting an error  :',
+      error
+    );
+    throw error;
+  }
+};
+
+
+export const handleSubscriptionDeleted = async (eventBody: any) => {
+  try {
+    console.log('test', eventBody);
+  } catch (error) {
+    console.log(
+      'Getting an error  :',
+      error
+    );
+    throw error;
+  }
+};
+
+
+export const handleInvoicePaymentSucceeded = async (eventBody: any) => {
+  try {
+    console.log('test', eventBody);
+  } catch (error) {
+    console.log(
+      'Getting an error  :',
+      error
+    );
+    throw error;
+  }
+};
+
+
+export const handleInvoicePaymentFailed = async (eventBody: any) => {
+  try {
+    console.log('test', eventBody);
+  } catch (error) {
+    console.log(
+      'Getting an error  :',
+      error
+    );
+    throw error;
+  }
+};
+
+
+export const handleInvoicePaid = async (eventBody: any) => {
+  try {
+    console.log('test', eventBody);
+  } catch (error) {
+    console.log(
+      'Getting an error  :',
+      error
+    );
+    throw error;
+  }
+};
+
+export const handlePaymentIntentSucceeded = async (eventBody: any) => {
+  try {
+    console.log('test', eventBody);
+  } catch (error) {
+    console.log(
+      'Getting an error  :',
+      error
+    );
+    throw error;
+  }
+};
+export const handlePaymentIntentFailed = async (eventBody: any) => {
+  try {
+    console.log('test', eventBody);
+  } catch (error) {
+    console.log(
+      'Getting an error  :',
+      error
+    );
+    throw error;
+  }
+};
+
+
 
 
--- src/services/webhook/webhook.provider.ts
+++ src/services/webhook/webhook.provider.ts
@@ -1,13 +1,32 @@
-import { Request, Response } from "express";
-import { stripe } from "../../config/stripe.config.js";
-import StripeConnectAccount from "../../models/stripeConnectAccount.model.js";
+import { Request, Response } from 'express';
+import { stripe } from '../../config/stripe.config.js';
+import StripeConnectAccount from '../../models/stripeConnectAccount.model.js';
+import {
+  handleInvoicePaid,
+  handleInvoicePaymentFailed,
+  handleInvoicePaymentSucceeded,
+  handleOneTimePaymentSuccess,
+  handlePaymentIntentFailed,
+  handlePaymentIntentSucceeded,
+  handleSubscriptionCreated,
+  handleSubscriptionDeleted,
+  handleSubscriptionSuccess,
+  handleSubscriptionUpdated,
+} from './webhook.helper.js';
 
 export const stripeWebhookPlatform = async (req: Request, res: Response) => {
   try {
-    console.log(req.headers);
-    const sig = req.headers["stripe-signature"] as string;
-    console.log("Event Secret :", process.env.STRIPE_WEBHOOK_SECRET);
+    // console.log(req.headers);
+    const sig = req.headers['stripe-signature'] as string;
+    // console.log('Event Secret :', process.env.STRIPE_WEBHOOK_SECRET);
     const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
+
+    if (!endpointSecret) {
+      console.error('WEBHOOK_SECRET is not configured');
+      res.status(500).json({ error: 'web-hook configuration error' });
+
+      return;
+    }
 
     let event: any;
 
@@ -15,60 +34,100 @@ export const stripeWebhookPlatform = async (req: Request, res: Response) => {
     try {
       event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
     } catch (err: any) {
-      console.log("Error from the signature fail", err);
+      console.log('Error from the signature fail', err);
       console.error(`Webhook signature verification failed: ${err.message}`);
-    }
+      res.status(400).json({ error: 'web-hook signature verification failed' });
 
-    if (!event && !event?.type) {
-      console.log("Event not found");
-      res.sendStatus(400);
       return;
     }
 
-    console.log("🚀 ~ stripeWebhook ~ event type:", event.type);
+    if (!event || !event?.type) {
+      console.log('Event not found');
+      res.status(400).json({ error: 'Event not found' });
+      return;
+    }
+
+    console.log('🚀 ~ stripeWebhook ~ event type:', event.type);
 
     let stripeObject = event?.data?.object;
-    var transactionType: string | null = null;
-    var status: string | null = null;
 
     switch (event?.type) {
-      case "checkout.session.completed": {
-        console.log(
-          "checkout.session.completed ........triggered........>>>>>"
-        );
-        console.log("💰 Checkout completed");
-        console.log("Session ID:", stripeObject.id);
-        console.log("Payment Status:", stripeObject.payment_status);
-        console.log("Customer:", stripeObject.customer);
-        console.log("PaymentIntent:", stripeObject.payment_intent);
-
-        /**
-         * 👉 DO YOUR DB WORK HERE
-         * - Mark order as PAID
-         * - Grant access to video / course
-         * - Save transaction
-         */
-
+      case 'checkout.session.completed':
+        if (stripeObject?.mode === 'payment') {
+          // one time payment success
+          await handleOneTimePaymentSuccess(stripeObject);
+        } 
+        // else if (stripeObject?.mode === 'subscription') {
+          //   await handleSubscriptionSuccess(stripeObject);
+          // }
         break;
-      }
+
+      // subscription
+      case 'customer.subscription.created':
+        await handleSubscriptionCreated(stripeObject);
+        break;
+
+      case 'customer.subscription.updated':
+        await handleSubscriptionUpdated(stripeObject);
+        break;
+
+      case 'customer.subscription.deleted':
+        await handleSubscriptionDeleted(stripeObject);
+        break;
+
+      // invoice
+      // case 'invoice.payment_succeeded':
+      //   await handleInvoicePaymentSucceeded(stripeObject);
+      //   break;
+
+      case 'invoice.payment_failed':
+        // This is for subscription payment failed
+        await handleInvoicePaymentFailed(stripeObject);
+        break;
+
+      // subscription payment success
+      case 'invoice.paid':
+        await handleInvoicePaid(stripeObject);
+        break;
+
+      // payment intent
+      // case 'payment_intent.succeeded':
+      //   await handlePaymentIntentSucceeded(stripeObject);
+      //   break;
+
+      case 'payment_intent.payment_failed':
+        if(stripeObject.invoice){
+          // invoice payment failed will handle by invoice.payment_failed
+          break;
+        }
+        // This is for one time payment failed
+        await handlePaymentIntentFailed(stripeObject);
+        break;
 
       default:
         console.log(`Unhandled event type: ${event.type}`);
     }
 
-    res.sendStatus(200);
-  } catch (err) {
-    console.log("🚀 ~ stripe webhook failed ~ err:", err);
-    res.sendStatus(400);
+    res.status(200).json({ received: true });
+  } catch (err: any) {
+    console.log('error in stripe web-hook', err);
+    res.status(400).json({ error: 'web-hook handler failed' });
   }
 };
 
 export const stripeWebhookConnect = async (req: Request, res: Response) => {
   try {
-    console.log(req.headers);
-    const sig = req.headers["stripe-signature"] as string;
-    console.log("Event Secret :", process.env.STRIPE_CONNECT_WEBHOOK_SECRET);
+    // console.log(req.headers);
+    const sig = req.headers['stripe-signature'] as string;
+    // console.log('Event Secret :', process.env.STRIPE_CONNECT_WEBHOOK_SECRET);
     const endpointSecret = process.env.STRIPE_CONNECT_WEBHOOK_SECRET as string;
+
+    if (!endpointSecret) {
+      console.error('WEBHOOK_SECRET is not configured');
+      res.status(500).json({ error: 'web-hook configuration error' });
+
+      return;
+    }
 
     let event: any;
 
@@ -76,40 +135,38 @@ export const stripeWebhookConnect = async (req: Request, res: Response) => {
     try {
       event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
     } catch (err: any) {
-      console.log("Error from the signature fail", err);
+      console.log('Error from the signature fail', err);
       console.error(`Webhook signature verification failed: ${err.message}`);
-    }
+      res.status(400).json({ error: 'web-hook signature verification failed' });
 
-    if (!event && !event.type) {
-      console.log("Event not found");
-      res.sendStatus(400);
       return;
     }
 
-    console.log("🚀 ~ stripeWebhook ~ event type:", event.type);
+    if (!event || !event?.type) {
+      console.log('Event not found');
+      res.status(400).json({ error: 'Event not found' });
+      return;
+    }
+
+    console.log('🚀 ~ stripeWebhook ~ event type:', event.type);
 
     let stripeObject = event.data.object;
-    var transactionType: string | null = null;
-    var status: string | null = null;
 
     switch (event?.type) {
-      case "account.updated":
-        console.log("account.updated ................");
-        console.log("🚀 ~ stripeWebhook ~ stripeObject:", stripeObject);
+      case 'account.updated':
         await StripeConnectAccount.findOneAndUpdate(
           { stripeAccountId: stripeObject.id },
           {
-            $set:{
-
-                chargesEnabled: stripeObject.charges_enabled,
-                payoutsEnabled: stripeObject.payouts_enabled,
-                detailsSubmitted: stripeObject.details_submitted,
-                isActive:
-                  stripeObject.charges_enabled &&
-                  stripeObject.payouts_enabled &&
-                  stripeObject.details_submitted,
-              }
-            }
+            $set: {
+              chargesEnabled: stripeObject.charges_enabled,
+              payoutsEnabled: stripeObject.payouts_enabled,
+              detailsSubmitted: stripeObject.details_submitted,
+              isActive:
+                stripeObject.charges_enabled &&
+                stripeObject.payouts_enabled &&
+                stripeObject.details_submitted,
+            },
+          }
         );
 
         break;
@@ -118,9 +175,9 @@ export const stripeWebhookConnect = async (req: Request, res: Response) => {
         console.log(`Unhandled event type: ${event.type}`);
     }
 
-    res.sendStatus(200);
-  } catch (err) {
-    console.log("🚀 ~ stripe webhook failed ~ err:", err);
-    res.sendStatus(400);
+    res.status(200).json({ received: true });
+  } catch (err: any) {
+    console.log('error in stripe web-hook', err);
+    res.status(400).json({ error: 'web-hook handler failed' });
   }
 };
