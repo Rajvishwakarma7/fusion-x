930dd489be9850f79a302cad03ffab9539352703
raj.vishwakarma <raj7280021@gmail.com>
2026-02-20 10:10:20 +0530

    feat: update socket connection

--- src/socket/socket.connection.ts
+++ src/socket/socket.connection.ts
@@ -4,44 +4,48 @@ import { Server } from 'socket.io';
 const onlineUsers = new Map();
 
 const initSocket = (io: Server) => {
+  try {
+    // socket authentication middleware
+    io.use(async (socket, next) => {
+      const token = socket.handshake.headers.auth as string;
+      const validToken = await decodeToken(token);
 
-  // socket authentication middleware
-  io.use(async (socket, next) => {
-    const token = socket.handshake.headers.auth as string;
-    const validToken = await decodeToken(token);
-
-    if (validToken) {
-      socket.data.user = validToken;
-      next();
-    } else {
-      logger.error('Invalid Token');
-      socket.disconnect(true);
-    }
-  });
-
-  // socket connection event
-  io.on('connection', (socket) => {
-    logger.info(`🟢 Socket connected: ${socket.id}`);
-    console.log('socket-id 🟢', socket.id, '😎', socket.data.user);
-
-    const userId = socket.data.user.userId;
-    onlineUsers.set(userId, socket.id);
-    
-
-    socket.on('message', (message) => {
-      console.log(
-        '-------------------------->>>> socket storage',
-        socket.data.user
-      );
-      console.log('message |||', message);
-      console.log('online----Users 🟢', onlineUsers);
-      io.emit('message', message);
+      if (validToken) {
+        socket.data.user = validToken;
+        next();
+      } else {
+        logger.error('Invalid Token');
+        socket.disconnect(true);
+      }
     });
 
-    socket.on('disconnect', () => {
-      logger.info(`🔴 Socket disconnected: ${socket.id}`);
+    // socket connection event
+    io.on('connection', (socket) => {
+      logger.info(`🟢 Socket connected: ${socket.id}`);
+      console.log('socket-id 🟢', socket.id, '😎', socket.data.user);
+
+      const userId = socket.data.user.userId;
+      onlineUsers.set(userId, socket.id);
+
+      socket.on('message', (message) => {
+        console.log(
+          '-------------------------->>>> socket storage',
+          socket.data.user
+        );
+        console.log('message |||', message);
+        console.log('online----Users 🟢', onlineUsers);
+        io.emit('message', message);
+      });
+
+      socket.on('disconnect', () => {
+        logger.info(`🔴 Socket disconnected: ${socket.id}`);
+        onlineUsers.delete(userId);
+      });
     });
-  });
+  } catch (error) {
+    console.error('Error initializing socket:', error);
+    throw error;
+  }
 };
 
 export { initSocket, onlineUsers };
